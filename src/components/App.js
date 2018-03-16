1c0b45088c4cbfe6e8f95e88536cc78608dc3719
Jeff Lee <jeffmlee@gmail.com>
2018-03-15 23:19:17 -0600

    lesson 21

--- src/components/App.js
+++ src/components/App.js
@@ -56,6 +56,15 @@ class App extends React.Component {
     this.setState({ fishes });
   };
 
+  deleteFish = (key) => {
+    // 1. take a copy of state
+    const fishes = { ...this.state.fishes };
+    // 2. update state
+    fishes[key] = null;
+    // 3. update state
+    this.setState({ fishes });
+  };
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
@@ -68,6 +77,16 @@ class App extends React.Component {
     // 3. Call setState to update our state object
     this.setState({ order });
   };
+
+  removeFromOrder = key => {
+    // 1. take a copy of state
+    const order = { ...this.state.order };
+    // 2. Either add to the order, or update the number in our order
+    order[key] = order[key];
+    // 3. Call setState to update our state object
+    this.setState({ order });
+  };
+  
 
   render() {
     return (
@@ -85,10 +104,15 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order 
+          fishes={this.state.fishes} 
+          order={this.state.order}
+          removeFromOrder={this.removeFromOrder} 
+        />
         <Inventory
           addFish={this.addFish}
-          updateFish={this.updateFish}          
+          updateFish={this.updateFish} 
+          deleteFish={this.updateFish}                   
           loadSampleFishes={this.loadSampleFishes}
           fishes={this.state.fishes}
         />
